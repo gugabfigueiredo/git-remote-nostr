1d6bea6de1cbb642ac84f8f6fc331ac3cf401bf3
Guga Figueiredo <gugabfigueiredo@gmail.com>
2024-06-01 21:53:49 +0200

    refactoring code around connect

--- main.go
+++ main.go
@@ -1,84 +1,79 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
-	"log"
-	"net/url"
+	"github.com/melbahja/goph"
 	"os"
-	"os/exec"
-	"regexp"
-	"time"
+	"strings"
+
+	golog "github.com/ipfs/go-log/v2"
 )
 
-func openLogFile() *os.File {
-	// Specify the log file path
-	logFile := "application.log"
-
-	// Attempt to open or create the log file
-	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-
-	// Set the output of the logger to the file
-	log.SetOutput(file)
-	return file
-}
-
-// parseRemoteURL parses the URL and returns the scheme if valid, otherwise an error.
-func parseRemoteURL(remoteUrl string) (string, error) {
-	if u, err := url.Parse(remoteUrl); err == nil && u.Scheme != "" {
-		return u.Scheme, nil
-	}
-
-	// Check for SCP-like SSH URLs first because they don't have a "scheme://"
-	scpRe := regexp.MustCompile(`^[a-zA-Z0-9._@/+~-]+@[a-zA-Z0-9._@/+~-]+:[a-zA-Z0-9._@/+~-]+(\.git)?$`)
-	if scpRe.MatchString(remoteUrl) {
-		return "ssh", nil
-	}
-
-	return "", errors.New("invalid Git URL scheme")
-}
-
-func Main() (er error) {
-
-	time.Sleep(30 * time.Second)
-	if len(os.Args) < 3 {
-		return fmt.Errorf("Usage: git-remote-nostr remote-name remote-url")
-	}
-
-	remoteName := os.Args[1]
-	remoteUrl := os.Args[2]
-	log.Printf("running git-remote-nostr: %s:%s\n", remoteName, remoteUrl)
-
-	scheme, err := parseRemoteURL(remoteUrl)
-	if err != nil {
-		return fmt.Errorf("Invalid remote URL %s: %v", remoteUrl, err)
-	}
-
-	switch scheme {
-	case "ssh":
-		return doSSH(remoteName, remoteUrl)
-	default:
-		return doRemoteHelper(remoteName, remoteUrl, scheme)
-	}
-}
-
-func doRemoteHelper(remoteName, remoteUrl, scheme string) error {
-	log.Printf("git %s %s %s", fmt.Sprintf("remote-%s", scheme), remoteName, remoteUrl)
-	cmd := exec.Command("git", fmt.Sprintf("remote-%s", scheme), remoteName, remoteUrl)
-	cmd.Env = append(os.Environ(), "GIT_TRACE_PACKET=1", "GIT_TRACE=1")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
-}
+var logger = golog.Logger("remote-helper")
 
 func main() {
-	logOut := openLogFile()
-	defer logOut.Close()
-
-	if err := Main(); err != nil {
-		log.Fatal(err)
+	args := os.Args
+	if len(args) < 3 {
+		logger.Fatal("Usage: git-remote-nostr <remoteName>")
 	}
+
+	stdinReader := bufio.NewReader(os.Stdin)
+	for {
+		command, err := stdinReader.ReadString('\n')
+		strings.Fields(command)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		switch {
+		case command == "capabilities\n":
+			fmt.Fprintln(os.Stdout, "connect")
+			fmt.Fprintln(os.Stdout, "")
+		case command == "connect git-upload-pack\n":
+			if err = DoConnect("git-upload-pack", ""); err != nil {
+				logger.Fatal(err)
+			}
+		case command == "connect git-receive-pack\n":
+			if err = DoConnect("git-receive-pack", ""); err != nil {
+				logger.Fatal(err)
+			}
+		default:
+			logger.Fatalf("Unknown command: %q", command)
+		}
+	}
+}
+
+func DoConnect(command, remote string) (err error) {
+	// Connects to given git service.
+
+	auth, err := goph.UseAgent()
+	if err != nil {
+		panic(err)
+	}
+
+	// Do something with auth
+	client, err := goph.New("git", "github.com", auth)
+	if err != nil {
+		panic(err)
+	}
+
+	// Do something with client
+	cmd, err := client.Command(command, "'gugabfigueiredo/git-remote-nostr.git'")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Stdout.WriteString("\n")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
+	return
 }
