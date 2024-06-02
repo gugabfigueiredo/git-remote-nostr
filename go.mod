71a02a2de14149abc4c3a5c4c3825b932af7e348
Guga Figueiredo <gugabfigueiredo@gmail.com>
2024-06-02 08:00:41 +0200

    working solution with ssh connection

--- go.mod
+++ go.mod
@@ -3,18 +3,15 @@ module github.com/gugabfigueiredo/git-remote-nostr
 go 1.22
 
 require (
-	github.com/ipfs/go-log/v2 v2.5.1
 	github.com/melbahja/goph v1.4.0
+	github.com/pkg/errors v0.9.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/fs v0.1.0 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/sftp v1.13.5 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -3,43 +3,66 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/melbahja/goph"
-	"os"
-	"strings"
 
-	golog "github.com/ipfs/go-log/v2"
+	"github.com/melbahja/goph"
+	"github.com/pkg/errors"
+	"io"
+	"log"
+	"os"
 )
 
-var logger = golog.Logger("remote-helper")
+var logOut *os.File
+
+func openLogFile() *os.File {
+	// Specify the log file path
+	logFile := "application.log"
+
+	// Attempt to open or create the log file
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	// Set the output of the logger to the file
+	log.SetOutput(file)
+	return file
+}
 
 func main() {
 	args := os.Args
 	if len(args) < 3 {
-		logger.Fatal("Usage: git-remote-nostr <remoteName>")
+		log.Fatal("Usage: git-remote-nostr <remoteName> <remoteUrl>")
 	}
+
+	logOut = openLogFile()
+	defer logOut.Close()
+
+	remoteUrl := args[2]
 
 	stdinReader := bufio.NewReader(os.Stdin)
 	for {
 		command, err := stdinReader.ReadString('\n')
-		strings.Fields(command)
+		if errors.Is(err, io.EOF) {
+			break
+		}
 		if err != nil {
-			logger.Fatal(err)
+			log.Fatal(err)
 		}
 
 		switch {
 		case command == "capabilities\n":
-			fmt.Fprintln(os.Stdout, "connect")
-			fmt.Fprintln(os.Stdout, "")
+			fmt.Println("connect")
+			fmt.Println()
 		case command == "connect git-upload-pack\n":
-			if err = DoConnect("git-upload-pack", ""); err != nil {
-				logger.Fatal(err)
+			if err = DoConnect("git-upload-pack", remoteUrl); err != nil {
+				log.Fatal(err)
 			}
 		case command == "connect git-receive-pack\n":
-			if err = DoConnect("git-receive-pack", ""); err != nil {
-				logger.Fatal(err)
+			if err = DoConnect("git-receive-pack", remoteUrl); err != nil {
+				log.Fatal(err)
 			}
 		default:
-			logger.Fatalf("Unknown command: %q", command)
+			log.Fatalf("Unknown command: %q", command)
 		}
 	}
 }
@@ -59,15 +82,15 @@ func DoConnect(command, remote string) (err error) {
 	}
 
 	// Do something with client
-	cmd, err := client.Command(command, "'gugabfigueiredo/git-remote-nostr.git'")
+	cmd, err := client.Command(command, fmt.Sprintf("'%s'", remote))
 	if err != nil {
 		panic(err)
 	}
-
-	os.Stdout.WriteString("\n")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
+	os.Stdout.WriteString("\n")
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
