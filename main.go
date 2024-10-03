a80c46449ae8c8954ca075767cda1c1c0b07cea1
Guga Figueiredo <gugabfigueiredo@gmail.com>
2024-10-03 20:05:18 +0200

    update main

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
 import (
-	"bufio"
-	"errors"
-	"fmt"
-	"github.com/gugabfigueiredo/git-remote-nostr/internal/domain"
 	"github.com/gugabfigueiredo/git-remote-nostr/internal/git"
 	"github.com/gugabfigueiredo/git-remote-nostr/internal/nostr"
-	"io"
+	"github.com/gugabfigueiredo/git-remote-nostr/internal/ssh"
 	"log"
 	"os"
-	"os/exec"
 )
 
-var nostrService domain.RemoteService
-
-func init() {
-	git.ProcessConfig()
-	pvtk, pubk := nostr.GetCredentials(git.Config.UserName, git.Config.UserEmail)
-	nostrClient := nostr.NewClient(pvtk, pubk)
-	nostrService = nostr.NewService(nostrClient)
-}
-
 func main() {
-
 	args := os.Args
 	if len(args) < 3 {
 		log.Fatal("Usage: git-remote-nostr <remoteName> <remoteUrl>")
 	}
+
+	git.Init()
+
+	pvtk, pubk := nostr.ResolveCredentials(git.Config.UserName, git.Config.UserEmail)
+	if pvtk == "" || pubk == "" {
+		log.Fatal("nostr environment is not properly configured")
+	}
+
+	nostrClient := nostr.NewClient(pvtk, pubk)
+	nostrService := nostr.NewService(nostrClient)
 
 	remote, err := nostrService.ResolveRemote(args[2])
 	if err != nil {
@@ -35,65 +30,19 @@ func main() {
 	}
 
 	switch remote.Protocol {
-	case "ssh", "nostr":
-		if err = nostrHelper(remote); err != nil {
+	case "nostr":
+		if err = nostrService.Helper(remote); err != nil {
+			log.Fatalf("failed to setup remote helper: %v", err)
+		}
+	case "ssh":
+		if err = ssh.Helper(remote); err != nil {
 			log.Fatalf("failed to setup remote helper: %v", err)
 		}
 	case "http", "https", "git":
-		if err = defaultHelper(args[1], remote); err != nil {
+		if err = git.Helper(args[1], remote); err != nil {
 			log.Fatalf("failed to setup remote helper: %v", err)
 		}
 	default:
 		log.Fatalf("unsupported protocol for remote url: %s", remote.String())
 	}
-}
-
-func doCMD(cmd *exec.Cmd) error {
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	fmt.Println()
-	return cmd.Run()
-}
-
-func defaultHelper(remoteName string, remote *domain.Remote) error {
-	cmd := exec.Command("git", "remote-"+remote.Protocol, remoteName, remote.String())
-	return doCMD(cmd)
-}
-
-func nostrHelper(remote *domain.Remote) error {
-	stdinReader := bufio.NewReader(os.Stdin)
-	for {
-		command, err := stdinReader.ReadString('\n')
-		if errors.Is(err, io.EOF) {
-			return nil
-		}
-		if err != nil {
-			return errors.Join(err, fmt.Errorf("failed to read command"))
-		}
-
-		switch {
-		case command == "capabilities\n":
-			fmt.Println("connect")
-			fmt.Println()
-		case command == "connect git-upload-pack\n":
-			if err := doConnect("git-upload-pack", remote); err != nil {
-				return err
-			}
-		case command == "connect git-receive-pack\n":
-			if err := nostrService.Auth(remote); err != nil {
-				return fmt.Errorf("failed to request upload: %v", err)
-			}
-			if err := doConnect("git-receive-pack", remote); err != nil {
-				return err
-			}
-		default:
-			return fmt.Errorf("unknown command: %s", command)
-		}
-	}
-}
-
-func doConnect(command string, remote *domain.Remote) error {
-	cmd := exec.Command("ssh", remote.Login(), command, remote.Path())
-	return doCMD(cmd)
 }
